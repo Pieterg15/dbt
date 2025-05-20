fbe7517556ec219f2c8f2a7adabdeae39fa272b0
Pieter Gouws <pietgouws15@gmail.com>
2025-05-20 17:57:54 +0000

    refactoring customer, orders and payment tables

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,11 +1,21 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with 
 
-    -- amount is stored in cents, convert it to dollars
-    {{ cents_to_dollars('amount', 4) }} as amount,
-    created as created_at
+source as (
 
-from {{ source('stripe', 'payment') }}
+    select * from {{ source('stripe', 'payment')}}
+
+),
+
+transformed as (
+
+    select 
+
+        id as payment_id,
+        orderid as order_id,
+        status as payment_status,
+        round(amount/100.0,2) as payment_amount
+    
+    from source
+)
+
+select * from transformed
